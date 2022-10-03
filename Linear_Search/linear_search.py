d982ba48dfa34b89853f46b6351f5faee8dfdb3d
imamcsembstu <imam.csembstu@gmail.com>
2022-10-03 17:21:54 +0600

    Linear_search Solution added

---
+++ Linear_Search/linear_search.py
@@ -0,0 +1,12 @@
+def linear_search(list_or_array,number_to_find):
+    n = len(list_or_array)
+
+    for i in range(n):
+        if list_or_array[i] == number_to_find:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    l=[23,3,5,7,11,4,55,9]
+    print(linear_search(l,1))
+
